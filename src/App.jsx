8c4809346b6a69e979dc4f9a9912bc6da72fc61f
Shadmans-World <shaon10516045@gmail.com>
2025-12-23 21:06:04 +0600

    project created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  
+
+  return <>
+  <h1 className="text-2xl font-bold text-center">Project Created</h1>
+  <h3 className="text-center">Hello World!</h3>
+  </>;
+}
+
+export default App;
