5f769d681c3768bf2e2e48f890ca4a7d10569e24
Shadmans-World <shaon10516045@gmail.com>
2025-12-28 19:19:42 +0600

    Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <Navbar />
-      <Outlet/>
-      
+      <div className="min-h-[calc(100vh-117px)]">
+        <Outlet/>
+      </div>
+      <Footer/>
+
     </>
   );
 }
